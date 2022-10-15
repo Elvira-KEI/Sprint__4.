1628b7592f2e7b17617043743d9afdbe3f53dd55
Elvira Kondrateva <113317619+Elvira-KEI@users.noreply.github.com>
2022-10-15 12:28:53 +1000

    My second commit
    -rename class
    -code formatting according to Code Style settings
    -changing the @Parameterized.Parameters annotation using the name argument

--- My_second_Project/src/test/java/WebTest.java
+++ My_second_Project/src/test/java/WebTest.java
@@ -9,43 +9,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.example.constantText.*;
-
+import static org.example.ConstantText.*;
 
 @RunWith(Parameterized.class)
 public class WebTest {
     WebDriver driver;
+    private final String constantText;
+    private final int index;
+    public WebTest(String constantText, int index) {
+        this.constantText = constantText;
+        this.index = index;
+    }
 
-        private final String constantText;
-        private final int index;
-        public WebTest(String constantText, int index) {
-            this.constantText = constantText;
-            this.index = index;
-       }
-        @Parameterized.Parameters
-        public static Object[][] getParameters() {
-            return new Object[][]{
-                    {ANSWER_TEXT_HOW_MUCH_AND_PAYMENT, 0},
-                    {ANSWER_TEXT_ORDERING_SEVERAL_SCOOTERS, 1},
-                    {ANSWER_TEXT_RENTAL_TIME, 2},
-                    {ANSWER_TEXT_ORDER_TODAY, 3},
-                    {ANSWER_TEXT_ORDER_EXTENSION_AND_RETURN, 4},
-                    {ANSWER_TEXT_SCOOTER_CHARGING, 5},
-                    {ANSWER_TEXT_ORDER_CANCELLATION, 6},
-                    {ANSWER_TEXT_DELIVERY_OUTSIDE_MOSCOW, 7},
-            };
-        }
+   @Parameterized.Parameters(name = "Тестовые данные: {0} {1} {2} {3} {4} {5} {6} {7}")
+   public static Object[][] getParameters() {
+       return new Object[][]{
+               {ANSWER_TEXT_HOW_MUCH_AND_PAYMENT, 0},
+               {ANSWER_TEXT_ORDERING_SEVERAL_SCOOTERS, 1},
+               {ANSWER_TEXT_RENTAL_TIME, 2},
+               {ANSWER_TEXT_ORDER_TODAY, 3},
+               {ANSWER_TEXT_ORDER_EXTENSION_AND_RETURN, 4},
+               {ANSWER_TEXT_SCOOTER_CHARGING, 5},
+               {ANSWER_TEXT_ORDER_CANCELLATION, 6},
+               {ANSWER_TEXT_DELIVERY_OUTSIDE_MOSCOW, 7},
+       };
+   }
+
     @Before
-    public void setUp(){
-      driver = new ChromeDriver();
-       // driver = new FirefoxDriver();
+    public void setUp() {
+        driver = new ChromeDriver();
+        // driver = new FirefoxDriver();
     }
 
     //Тест для выпадающего списка в разделе «Вопросы о важном».
     @Test
-    public void checkClickArrow_openСorrespondingText(){
-       MainPage mainPage = new MainPage(driver);
+    public void checkClickArrow_openСorrespondingText() {
+        MainPage mainPage = new MainPage(driver);
         mainPage.open();
         new WebDriverWait(driver, 5);
         mainPage.clickCloseCookies();
